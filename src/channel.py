c325325733e5346604bccebec7ec7ff37edefc67
G <0020992@gmail.com>
2023-09-11 21:35:28 +0300

    1. Добавление класса Channel: Добавлен класс Channel в модуль src.channel, предназначенный для получения информации о YouTube-каналах.
        2. Создание скрипта main.py: Создан скрипт main.py, который использует класс Channel для получения и вывода информации о канале YouTube.
    
        3. Добавление Документации: Добавлена документация (докстринги) к классу Channel и скрипту main.py, описывающая их функциональность и использование.
    
        4. Установка переменной окружения YOUTUBE_API_KEY: Добавлена информация о необходимости установки переменной окружения YOUTUBE_API_KEY с ключом API YouTube для корректной работы скрипта.
    
        5. Изменение скрипта main.py: В скрипте main.py добавлена функция main(), позволяющая запускать скрипт из командной строки.
    
        6. Обновление Документации: Обновлена документация к скрипту main.py с учетом изменений в его структуре.
    
        7. Исправление скрипта main.py: В скрипте main.py добавлен вывод идентификатора канала YouTube в информации о канале.
    
        8. Добавление функции main(): Добавлена функция main() для более удобного управления запуском скрипта main.py.
    
        9. Обновление коммит-сообщений: Обновлены коммит-сообщения для отражения всех сделанных изменений.

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,60 @@
-
+# print(os.getenv("YOUTUBE_API_KEY"))
+#
+# # src/channel.py
+import os
+import requests
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """
+    Класс для работы с YouTube каналом.
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+    Args:
+        channel_id (str): ID YouTube канала.
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+    Attributes:
+        channel_id (str): ID YouTube канала.
+        api_key (str): Ключ API для доступа к YouTube Data API.
+
+    Methods:
+        print_info(): Получает и выводит информацию о канале.
+
+    Examples:
+        Создание объекта канала и получение информации о нем:
+        >>> moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+        >>> moscowpython.print_info()
+    """
+    def __init__(self, channel_id):
+        """
+        Инициализирует объект канала с заданным ID.
+
+        Args:
+            channel_id (str): ID YouTube канала.
+        """
+        self.channel_id = channel_id
+        self.api_key = os.getenv("YOUTUBE_API_KEY")
+
+    def print_info(self):
+        """
+       Получает и выводит информацию о канале, включая ID.
+
+       Returns:
+           None
+        """
+        if not self.api_key:
+            print("Не установлен ключ API YouTube.")
+            return
+
+        url = f"https://www.googleapis.com/youtube/v3/channels?key={self.api_key}&id={self.channel_id}&part=snippet,statistics"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            channel_data = data.get("items")[0]
+
+            print(f"Информация о канале: {channel_data['id']}")
+            print(f"Название: {channel_data['snippet']['title']}")
+            print(f"Описание: {channel_data['snippet']['description']}")
+            print(f"Количество подписчиков: {channel_data['statistics']['subscriberCount']}")
+            print(f"Количество просмотров: {channel_data['statistics']['viewCount']}")
+        else:
+            print("Ошибка при получении данных о канале.")
