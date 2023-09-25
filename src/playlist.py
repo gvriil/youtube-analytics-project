733d0f4cc1d7e9964fde426680e07492fb890bed
G <0020992@gmail.com>
2023-09-26 00:49:29 +0300

    15.2
    Added the PlayList class for working with YouTube playlist data and the parse_duration function.
    
    Description of changes:
    - Created the PlayList class for managing YouTube playlist data.
    - Implemented the parse_duration function to parse video durations in the format PT1H23M45S.
    
    Testing the generated link:
    - Initialized a PlayList object with a YouTube playlist ID.
    - Printed the result of pl.show_best_video() to verify the best video URL.
    - Displayed the best video URL.

--- src/playlist.py
+++ src/playlist.py
@@ -1,13 +1,25 @@
 import os
-import requests
 from datetime import timedelta
+
+import requests
+
+from src.video import Video
 
 # Получаем ключ API YouTube из переменных окружения
 api_key = os.getenv("YOUTUBE_API_KEY")
 
 
 class PlayList:
+    """
+    Класс для работы с данными о плейлисте YouTube.
+    """
+
     def __init__(self, playlist_id):
+        """
+        Инициализация объекта класса PlayList.
+
+        :param playlist_id: Идентификатор плейлиста YouTube.
+        """
         self.playlist_id = playlist_id
         self.title = ""
         self.url = ""
@@ -20,6 +32,9 @@ class PlayList:
         self.get_playlist_info()
 
     def get_playlist_info(self):
+        """
+        Получает информацию о плейлисте из YouTube API и обновляет атрибуты объекта.
+        """
         url = f"https://www.googleapis.com/youtube/v3/playlists?key={api_key}&id={self.playlist_id}&part=snippet"
         response = requests.get(url)
 
@@ -36,6 +51,9 @@ class PlayList:
             print("Ошибка при получении данных о плейлисте.")
 
     def get_playlist_videos(self):
+        """
+        Получает информацию о видео в плейлисте из YouTube API и обновляет список видео в атрибуте объекта.
+        """
         url = f"https://www.googleapis.com/youtube/v3/playlistItems?key={api_key}&playlistId={self.playlist_id}&part=snippet"
         response = requests.get(url)
 
@@ -52,47 +70,33 @@ class PlayList:
 
     @property
     def total_duration(self):
+        """
+        Возвращает суммарную продолжительность видео в плейлисте в формате timedelta.
+
+        :return: Суммарная продолжительность видео в плейлисте.
+        :rtype: timedelta
+        """
         total_duration_seconds = sum(video.duration.total_seconds() for video in self.videos)
         return timedelta(seconds=total_duration_seconds)
 
     def show_best_video(self):
+        """
+        Возвращает ссылку на самое популярное видео в плейлисте (по количеству лайков).
+
+        :return: Ссылка на самое популярное видео в плейлисте.
+        :rtype: str
+        """
         if not self.videos:
             return None
         best_video = max(self.videos, key=lambda video: video.likes)
-        return best_video.url
 
+        # Извлечь video_id из полного URL видео
+        video_id = best_video.video_id
 
-class Video:
-    def __init__(self, video_id):
-        self.video_id = video_id
-        self.title = ""
-        self.url = ""
-        self.views = 0
-        self.likes = 0
-        self.duration = timedelta()
+        # Собрать новый URL в формате "https://youtu.be/{video_id}"
+        best_video_url = f"https://youtu.be/{video_id}"
 
-        if not api_key:
-            print("Не установлен ключ API YouTube.")
-            return
-
-        self.get_video_info()
-
-    def get_video_info(self):
-        url = f"https://www.googleapis.com/youtube/v3/videos?key={api_key}&id={self.video_id}&part=snippet,statistics,contentDetails"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            data = response.json()
-            video_data = data.get("items")[0]
-
-            self.title = video_data['snippet']['title']
-            self.url = f"https://www.youtube.com/watch?v={self.video_id}"
-            self.views = int(video_data['statistics']['viewCount'])
-            self.likes = int(video_data['statistics']['likeCount'])
-            duration = video_data['contentDetails']['duration']
-            self.duration = parse_duration(duration)
-        else:
-            print("Ошибка при получении данных о видео.")
+        return best_video_url
 
 
 def parse_duration(duration_str):
@@ -110,3 +114,10 @@ def parse_duration(duration_str):
         seconds = int(duration_str.replace('S', ''))
 
     return timedelta(hours=hours, minutes=minutes, seconds=seconds)
+
+
+# тестируем полученную ссылку
+pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
+print(f"{pl.show_best_video}")
+best_video_url = pl.show_best_video()
+print("Best Video URL:", best_video_url)
